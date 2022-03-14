0c4b250b8df91c2b5586a1daf3b737056a43b1bb
Mihaisum <101585991+Mihaisum@users.noreply.github.com>
2022-03-14 21:17:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Evidenta_studenti
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nClasa Student");
+         
+            Console.ReadKey();
+
+        
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evidenta_studenti
+{
+    public class Student
+    {
+       /* //constanta
+        private const bool SUCCES = true;
+
+        //data membra privata
+        int[] note;
+
+        public static int IDStudent { get; set; };
+        public string Nume { get; set };
+        public string Prenume { get; set };
+
+        public Student(string _nume,string _prenume)
+        {
+            Nume = _nume;
+            Prenume = _prenume;
+
+        }
+
+       /* public Student(string _nume,string _prenume, int [] _note)
+        {
+            Nume = _nume;
+            Prenume = _prenume;
+            note = new int[_note.Length];
+            _note.CopyTo(note, 0);
+          
+        }
+       */
+       
+
+
+    }
+}
